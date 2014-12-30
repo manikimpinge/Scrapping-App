7f4151ca70befa30dee6388082c7525a06ee7f29
sanjeev <sanjeev.impinge@gmail.com>
2014-12-30 17:54:44 +0530

    Initial commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,24 @@
+class HomeController < ApplicationController
+  def index
+  	if params[:data]
+  		@doc = eval(params[:data])
+  		session[:data] = @info
+  		# render text: @info.to_csv
+  	end	
+  	# respond_to do |format|
+   #    format.html
+   #    format.csv { render text: @info.to_csv } if @info
+   #    binding.pry
+   #  end
+  end
+
+  def show_result
+    @selected_data = session[:data]
+    respond_to do |format|
+      format.html
+      format.csv { send_data @selected_data.to_csv }
+      #format.xls  #{ send_data @selected_data.to_csv(col_sep: "\t"),filename: params[:name] }
+    end
+  end
+
+end
